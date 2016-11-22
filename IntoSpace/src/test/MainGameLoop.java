709030a15ee2ad032757fb02aecdb8c23473bb0a
dfb159 <jSigrist@web.de>
2016-11-22 19:37:28 +0100

    libs setup lwjgl and slick-utils; added opengl Display

---
+++ IntoSpace/src/test/MainGameLoop.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		DisplayManager.createDisplay();
+		
+		while(!Display.isCloseRequested()) {
+			//gamelogic
+			//render
+			DisplayManager.updateDisplay();
+		}
+		
+		DisplayManager.closeDisplay();
+	}
+
+}
