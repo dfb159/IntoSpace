1b2457d7b1b1dc7713af721e7ae79fb3f07407cb
dfb159 <jSigrist@web.de>
2016-11-22 18:25:11 +0100

    Changing Hello World

--- IntoSpace/src/test/HelloWorld.java
+++ IntoSpace/src/test/HelloWorld.java
@@ -4,8 +4,12 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		System.out.println(getHelloWorld());
 
+	}
+	
+	public static String getHelloWorld() {
+		return new String("Hello World!");
 	}
 
 }
